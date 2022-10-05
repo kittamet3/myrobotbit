74e0b3032e2e51c6aa8879f1e7ee64e82fe73588
kittamet3 <114392629+kittamet3@users.noreply.github.com>
2022-10-06 03:03:01 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-    while (true) {
+    while (false) {
         OLED.clear()
         OLED.writeString("P0=")
-        OLED.writeNum(MySensor.digitalRead(MySensor.digitalPort.P0))
+        OLED.writeNum(MySensor.analogRead(MySensor.analogPort.P0))
         OLED.writeString("<+++>")
         OLED.writeString("P1=")
-        OLED.writeNum(MySensor.digitalRead(MySensor.digitalPort.P1))
+        OLED.writeNum(MySensor.analogRead(MySensor.analogPort.P1))
         OLED.writeString("<+++>")
         OLED.writeString("P2=")
-        OLED.writeNum(MySensor.digitalRead(MySensor.digitalPort.P2))
+        OLED.writeNum(MySensor.analogRead(MySensor.analogPort.P2))
         OLED.writeString("<+++>")
         OLED.writeString("P3")
-        OLED.writeNum(MySensor.digitalRead(MySensor.digitalPort.P3))
+        OLED.writeNum(MySensor.analogRead(MySensor.analogPort.P3))
         OLED.writeString("<+++>")
         basic.pause(200)
     }
 })
 function Linefollow () {
-	
+    Loop = true
+    ค่าแสงP0 = 0
+    ค่าแสงP1 = 0
+    ค่าแสงP2 = 0
+    ค่าแสงP3 = 0
+    if (true) {
+    	
+    } else {
+    	
+    }
 }
 function ไป () {
 	
 }
+let ค่าแสงP3 = 0
+let ค่าแสงP2 = 0
+let ค่าแสงP1 = 0
+let ค่าแสงP0 = 0
+let Loop = false
 led.enable(false)
 OLED.init(128, 64)
-while (!(input.buttonIsPressed(Button.AB))) {
+while (!(input.buttonIsPressed(Button.A))) {
 	
 }
-ไป()
